65358f94a227c80d75b18061d54e8cf29dbabd06
Abu Bakar Siddique Arman <arman.bhaai@gmail.com>
2018-07-30 23:20:54 +0600

    Added "bangla-text" class to pages for Bangla Font Fix

--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -12,7 +12,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('bangla-text'); ?>>
 	<?php
 	if ( is_sticky() && is_home() ) :
 		echo twentyseventeen_get_svg( array( 'icon' => 'thumb-tack' ) );
