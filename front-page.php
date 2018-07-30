272e99ce9cdca9fd692c267cc623539596a1c461
Abu Bakar Siddique Arman <arman.bhaai@gmail.com>
2018-07-30 14:25:08 +0600

    Removed Front-Page Contents

--- front-page.php
+++ front-page.php
@@ -17,14 +17,6 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 
-		<?php // Show the selected frontpage content.
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else :
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
 
 		<?php
 		// Get each of our panels and show the post data.
